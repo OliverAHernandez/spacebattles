c06d3e97177303570701106cad7f32e4f75e2774
airforceoli <oabalbuena@gmail.com>
2019-09-05 12:46:38 -0400

    Added first version of the ammo class

---
+++ ammo.py
@@ -0,0 +1,22 @@
+class Ammunition:
+    def __init__(self,ammo_speed, ammo_image,ammo_size, ammo_damage):
+        self.ammo_speed = ammo_speed
+        self.ammo_image = ammo_image
+        self.ammo_size = ammo_size
+        self.ammo_damage = ammo_damage
+
+    def increaseFire(self):
+        return 2 * self.ammo_speed
+
+    def tripleFire(self):
+        bullet1,bullet2,bullet3 = self.ammo_image
+        return(bullet1,bullet2,bullet3)
+
+
+    def laser(self):
+        self.ammo_image = "insert lazer image"
+        self.ammo_damage = "max damage"
+        self.ammo_speed = 'changed'
+
+
+    def
